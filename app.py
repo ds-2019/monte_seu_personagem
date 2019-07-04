7c8644e6fe9c671bc60701c24969e58ad372d376
mrodrigues <matheus.1nascimento58@gmail.com>
2019-07-04 12:13:57 -0300

    Criado pagina para o personagem

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
+@app.route('/personagem', methods=['POST'])
+def personagem():
+    personagem=request.form
+    return render_template('personagem.html', personagem=personagem)
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
